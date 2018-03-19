a2fa8b4bc1246b46ad6b8f38ce6ff6edff372ae1
min2d <min2dreamin@gmail.com>
2018-03-19 09:41:04 +0900

    keypressを追加した

--- web_ts/src/Omote.ts
+++ web_ts/src/Omote.ts
@@ -1,5 +1,18 @@
 export default class Omote extends Phaser.State {
   create(){
     console.log('Omote created');
+		this.input.keyboard.addCallbacks(this, null, null, this.keyPress);    
   }
+  keyPress() {
+		console.log(this.game.input.keyboard.event.keyCode);
+		console.log(this.game.input.keyboard.event.key);
+		//Phaser.Keycode.A=数字みたいな宣言もあるっぽ
+		//エンターは取れる。タブが取れない。
+		//おそらくeventの中身はJSのKeyboardEventと同じもの
+
+		var sfx = this.add.audio('sfx01');
+		sfx.play();
+		
+	}
+
 }
